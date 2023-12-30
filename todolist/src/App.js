250c67c547cee79284c59574367a0fa36c9fec64
Ronaldyika <buhnyuyronald@gmail.com>
2023-12-30 18:39:51 +0100

    started the outline of the todo

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -4,20 +4,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Yika Todos</h1>
+      <div className='todo-wrapper'>
+        <div className='todo-input'>
+          <div className='todo-input-item'>
+            <label>Title</label>
+            <input type='text' placeholder='enter a title'/>
+          </div>
+          <div className='todo-input-item'>
+            <label>Description</label>
+            <input type='text' placeholder='enter a description'/>
+          </div>
+          <div className='todo-input-item'>
+            <button className='primaryBtn'>Add</button>
+          </div>
+        </div>
+        <div className='btn-area'>
+          <button>Todo</button>
+          <button>Completed</button>
+        </div>
+        <div className='todo-list'>
+          <div className='todo-list-item'>
+            <h1>Task 1</h1>
+            <p>description</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
