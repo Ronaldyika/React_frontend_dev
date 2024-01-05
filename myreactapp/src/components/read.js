a9e0e7542bf1ba5d250cf75ca6b2c4950b38bdc0
Ronaldyika <buhnyuyronald@gmail.com>
2024-01-05 19:45:35 +0100

    studied stateless arrow functions and normal functions

---
+++ myreactapp/src/components/read.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Greet(){
+    return (
+        <div>Hello Ronald</div>
+    );
+}
+
+
+// using arror functions
+
+const Greeting =()=> <h2>hello ronald </h2>
+
+
+
+
+const salute =()=>{
+    return(
+        <div>
+            <h2> Ronald Yika</h2>
+            <p3>all of this will not matter</p3>
+        </div>
+    );
+}
+
+export default salute;
