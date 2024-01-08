8ee56c081cfb15acddf1d0b366b208f9ba03265b
Ronaldyika <buhnyuyronald@gmail.com>
2024-01-08 05:22:41 +0100

    started form validation with react components and 'onchange' event

---
+++ ReactProjects_folder/formvalidation/src/components/formcomponent.js
@@ -0,0 +1,28 @@
+// import React,{component} from 'react'
+
+
+// class FormDetail extends component{
+//     constructor(props){
+//         super(props)
+//         this.state = ({
+//             message:""
+//         })
+//     }
+
+//     userNameHandler =(event)=>{
+//         this.setState({
+//             message:{event.target.value}
+//         })
+//     }
+//     render(){
+//         return(
+//             <div>
+//                 <form>
+//                     <label>Username</label>
+//                     <input type='text' placeholder='enter your username' value={this.state.username} onChange={()=>this.userNameHandler}/>
+//                 </form>
+//             </div>
+//         )
+//     }
+
+// }
